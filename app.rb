29fc8baace72affd6cf8d1648b8ae91df9dece94
Vlad Mehakovic <vladiim@yahoo.com.au>
2015-01-17 13:13:27 +1100

    Processes markdown file

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'haml'
+require 'ostruct'
+require 'redcarpet'
+require 'byebug'
 
 class App
   attr_reader :haml
@@ -7,13 +10,54 @@ class App
     @haml = File.read(File.open('index.haml', 'r'))
   end
 
-  def html
-    Haml::Engine.new(haml).render
-  end
-
   def render_index_html
     index = File.open('index.html', 'w')
     index << html
     index.close
+  end
+
+  private
+
+  def html
+    Haml::Engine.new(haml).render( Object.new, folio: folio )
+  end
+
+  def folio
+    [Parser.new("#{ Dir.pwd }/folio/test.md").data]
+  end
+end
+
+class Parser
+  attr_reader :meta, :body
+
+  def initialize(file)
+    file  = File.open(file, 'r')
+    split = file.read.split('----')
+    @meta = parse_meta(split[1])
+    @body = parse_body(split[2])
+  end
+
+  def data
+    return meta if meta.body
+    meta.body = body
+    meta
+  end
+
+  private
+
+  def parse_meta(string)
+    meta_split = string.split(/\n/)
+    OpenStruct.new(
+      title:        meta_split[1].match(/: (.+)/)[1],
+      description:  meta_split[2].match(/: (.+)/)[1],
+      date:         meta_split[3].match(/: (.+)/)[1],
+      category:     meta_split[4].match(/: (.+)/)[1],
+      image:        meta_split[5].match(/: (.+)/)[1]
+    )
+  end
+
+  def parse_body(string)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = { strikethrough: true })
+    markdown.render(string)
   end
 end
